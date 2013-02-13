5491d2d6078f422375b0c63b9b290eae024303d1
Franck Sabattier <franck.sabattier@gmail.com>
2013-02-13 12:16:07 +0100

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+ruby '1.9.3'
+gem 'rails', '3.2.12'
+gem 'haml'
+gem 'jquery-rails'
+
+group :production do
+  gem 'google-analytics-rails'
+end
+
+group :development do
+  gem 'heroku'
+end
+
+group :assets do
+  gem 'sass-rails'
+  gem 'bootstrap-sass', '2.2.2.0'
+  gem 'font-awesome-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teasers::Application.config.secret_token = '4cfc4c016757496c00f18e309e0f72ce8e2ad4d41bb45676824f39f45fe5278c266eb73eadc23d4a53a8609e11bdf31ec87b53c2daf492861e2fcd84f63ccf3c'
