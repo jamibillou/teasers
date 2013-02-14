52916dc7faecbccf3e462f101a6181cb4c9b515b
Dominic Matheron <d.matheron@gmail.com>
2013-02-14 08:44:22 +0100

    Fixed routes constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
-require File.expand_path('../../lib/routes_constraints', __FILE__)
-
 Teasers::Application.routes.draw do
 
   match 'join',   to: 'pages#join'
   match 'signup', to: 'pages#signup'
 
   # Dynamic root_path
-  root to: 'pages#join'#,   constraints: Signup.new(false)
-  #root to: 'pages#signup', constraints: Signup.new(true) 
+  root to: 'pages#join',   constraints: {subdomain: 'join'}
+  root to: 'pages#signup', constraints: {subdomain: 'signup'}
 end
--- lib/routes_constraints.rb
+++
@@ -1,5 +0,0 @@
-class Signup < Struct.new(:value)
-	def matches?(request)
-		request.subdomain.present? && request.subdomain.match(/^signup$/)
-	end
-end
